05e8a749b0cc19af7f7a2274bb34eb666e0e938f
phuju <fozanmanzoor91@gmail.com>
2025-04-20 20:41:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,23 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 latest_data = {}
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
     return "Flask server is active"
-
-@app.route('/data', methods=['GET'])
-def get_data():
-    return jsonify(latest_data)
 
 @app.route('/send-data', methods=['POST'])
 def receive_data():
     global latest_data
     data = request.get_json()
+    print("Received data:", data)
     if data:
-        print("Received:", data)
         latest_data = data
-        return jsonify({'status': 'success'}), 200
-    else:
-        return jsonify({'status': 'no data received'}), 400
+        return jsonify({'status': 'received'}), 200
+    return jsonify({'status': 'error'}), 400
+
+@app.route('/data', methods=['GET'])
+def send_data():
+    return jsonify(latest_data)
+
+if __name__ == '__main__':
+    app.run()
